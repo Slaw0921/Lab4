38a74abe0d51c5706af83b355794ca6199721a88
Slaw0921 <160153556+Slaw0921@users.noreply.github.com>
2024-03-14 09:24:50 -0500

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,17 @@
+	function popupfunction(x)
+	{
+		document.getElementById('popup'+x).style.display='block';
+		document.getElementById('fade'+x).style.display='block';
+	}
+	function popupclose(x)
+	{
+		document.getElementById('popup'+x).style.display='none';
+		document.getElementById('fade'+x).style.display='none';
+	}
+	function addToCart(x)
+	{
+		document.getElementById("MyCart").innerHTML += x + "<br>";
+	}
+	function displayCart(){
+		document.getElementById("MyCart").style.display ='block';
+	}
